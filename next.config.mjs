3004835b5c894a31a7b4c2b241d6c176320c30a5
Bhavesh Songara <bhaveshsongara143@gmail.com>
2024-10-02 22:40:33 +0530

    Added Redirect from home to chat

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/chat",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
